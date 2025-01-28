0c752b7893f3bbf5510c026aab0935431df319a9
xqtkk <suusslauaa@gmail.com>
2025-01-28 11:56:08 +0500

    need some front rework

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"forum/database"
 	"forum/handlers"
-	_ "github.com/mattn/go-sqlite3"
 	"log"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
@@ -15,18 +16,19 @@ func main() {
 		log.Fatal(err)
 	}
 	defer db.Close()
+
 	// Создание категорий и постов (если нужно)
 	database.CreateCategory(db, "General")
 	database.CreateCategory(db, "Technology")
-	//err = database.CreateUser(db, "user@example.com", "username123", "password123")
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//err = database.CreatePost(db, "First Post", "This is the content of the first post.", 1, 1, time.Now().Format("2006-01-02 15:04:05"), 1)
-	//if err != nil {
-	//	print("sosal?")
-	//	log.Fatal(err) // Логируем и завершаем программу в случае ошибки
-	//}
+	// err = database.CreateUser(db, "user@example.com", "username123", "password123")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// err = database.CreatePost(db, "First Post", "This is the content of the first post.", 1, 1, time.Now().Format("2006-01-02 15:04:05"), 1)
+	// if err != nil {
+	// 	print("sosal?")
+	// 	log.Fatal(err) // Логируем и завершаем программу в случае ошибки
+	// }
 
 	//err = database.CreatePost(db, "Second Post", "This is the content of the second post.", 2, 2, time.Now().Format("2006-01-02 15:04:05"))
 	//if err != nil {
